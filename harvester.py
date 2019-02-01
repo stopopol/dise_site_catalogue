46b7fadbe53fc6667b85977bbf05f83716be65a8
stopopol <christoph.wohner@umweltbundesamt.at>
2019-02-01 14:50:53 +0100

    Update harvester.py

--- harvester.py
+++ harvester.py
@@ -10,9 +10,9 @@ with open('sites.csv', mode='wb') as sites_file:
     site_writer = csv.writer(sites_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     site_writer.writerow(["site_name", "site_description", "geom",'ri'])
 
-deims_path = os.path.dirname(os.path.abspath(__file__)) + "\harvester\deims.py"
-icos_path = os.path.dirname(os.path.abspath(__file__))  + "\harvester\icos.py"
-nemsr_path = os.path.dirname(os.path.abspath(__file__)) + "\harvester\_nemsr.py"
+deims_path = os.path.dirname(os.path.abspath(__file__)) + "\harvester\harvest_deims.py"
+icos_path = os.path.dirname(os.path.abspath(__file__))  + "\harvester\harvest_icos.py"
+nemsr_path = os.path.dirname(os.path.abspath(__file__)) + "\harvester\harvest_nemsr.py"
 
 subprocess.Popen(deims_path, shell=True)
 subprocess.Popen(icos_path, shell=True)
