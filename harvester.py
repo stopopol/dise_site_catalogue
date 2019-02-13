56d285d04a0fe99bea733e283f34c453c1a99523
stopopol <christoph.wohner@umweltbundesamt.at>
2019-02-13 10:47:47 +0100

    Update harvester.py

--- harvester.py
+++ harvester.py
@@ -6,15 +6,16 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # write one big csv including site information from all catalogues
-#reset csv here
+# reset csv here
 with open('sites.csv', mode='wb') as sites_file:
     site_writer = csv.writer(sites_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    site_writer.writerow(["site_name", "geom",'catalogue', "site_description"])
+    site_writer.writerow(["WKT", "country", "id", "site_name", 'catalogue', "site_description"])
 
 deims_path = os.path.dirname(os.path.abspath(__file__)) + "\harvester\harvest_deims.py"
 icos_path = os.path.dirname(os.path.abspath(__file__))  + "\harvester\harvest_icos.py"
 nemsr_path = os.path.dirname(os.path.abspath(__file__)) + "\harvester\harvest_nemsr.py"
 
+subprocess.Popen(nemsr_path, shell=True)
 subprocess.Popen(deims_path, shell=True)
 subprocess.Popen(icos_path, shell=True)
-subprocess.Popen(nemsr_path, shell=True)
+
