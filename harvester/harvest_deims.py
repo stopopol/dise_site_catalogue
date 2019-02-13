d3739e6f63fc58655ed577b1def6417ede16681c
stopopol <christoph.wohner@umweltbundesamt.at>
2019-02-13 10:46:58 +0100

    Update harvest_deims.py

--- harvester/harvest_deims.py
+++ harvester/harvest_deims.py
@@ -2,9 +2,15 @@
 #
 # https://www.deims.org/emf/harvest/json
 import csv
+import time
+import datetime
 import urllib, json
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
+# https://operations.osmfoundation.org/policies/nominatim/
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="specify_your_app_name_here")
+from geopy.exc import GeocoderTimedOut
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -20,11 +26,14 @@ with open('sites.csv', mode='ab') as sites_file:
 
     for x in data['nodes']:
         # limit the amount of processed records
-        #counter = counter+1
+        counter = counter+1
         #if counter == 50:
         #   break    
         #print(x['node']['deimsid'] + '; ' + x['node']['path'])
         #print x['node']['path']
+
+        # DEIMS.iD
+        site_id = x['node']['deimsid'];
 
         response = urllib.urlopen(x['node']['path'])
         e = xml.etree.ElementTree.parse(urllib.urlopen(x['node']['path'])).getroot()
@@ -46,9 +55,34 @@ with open('sites.csv', mode='ab') as sites_file:
                     if (len(coordinate_pair) == 2):
                         lat = coordinate_pair[0]
                         lon = coordinate_pair[1]
+                        print site_name
+                        if not lat:
+                            continue
+                        if not lon:
+                            continue
                         wkt_coordinates = "POINT (" + lon + " " + lat + ")"
-                        site_writer.writerow([site_name, wkt_coordinates,'deims', site_description])
-        
+                        # Country
+                        ready_coords = [lat, lon]
+                        try:
+                            print ready_coords
+                            ts = time.time()
+                            st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                            print st
+                            #country = "Phantasia"
+                            location = geolocator.reverse(ready_coords, language='en')
+                            country = location.raw['address']['country']
+
+                        except GeocoderTimedOut:
+                            country = "timeout"
+                        except KeyError:
+                            country = "undefined"
+                        
+                        site_writer.writerow([wkt_coordinates, country, site_id, site_name,'deims', site_description])
+                        time.sleep(2)
+
+    print "Processed DEIMS-SDR records: " + str(counter)
+
+
 
     #{http://inspire.ec.europa.eu/schemas/ef/4.0}responsibleParty"):
     #{http://inspire.ec.europa.eu/schemas/ef/4.0}responsibleParty"):
