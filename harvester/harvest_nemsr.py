0569ab532ab1b6077a5569bc720e379d752969eb
stopopol <christoph.wohner@umweltbundesamt.at>
2019-02-13 10:47:28 +0100

    Update harvest_nemsr.py

--- harvester/harvest_nemsr.py
+++ harvester/harvest_nemsr.py
@@ -2,6 +2,7 @@
 #
 # http://www.neii.gov.au/nemsr/using
 
+# https://operations.osmfoundation.org/policies/nominatim/
 import urllib, json, csv
 
 list_of_subnetworks = []
@@ -12,20 +13,28 @@ list_of_subnetworks.append("http://neii.bom.gov.au/cgi-bin/nemsr/get_geojson.py?
 # ...
 list_of_subnetworks.append("http://neii.bom.gov.au/cgi-bin/nemsr/get_geojson.py?network_id=28")
 
+total_count = 0
 for url in list_of_subnetworks:
 
   response = urllib.urlopen(url)
   data = json.loads(response.read())
 
-  
+  counter = 0
   with open('sites.csv', mode='ab') as sites_file:
     site_writer = csv.writer(sites_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
     for x in data['features']:
+      counter = counter+1
       site_name = x['properties']['name']
       site_description = x['properties']['siteDescription']
       lat  = x['properties']['latitude']
       lon  = x['properties']['longitude']
       wkt_coordinates = "POINT (" + lon + " " + lat + ")"
+
+      site_id = x['properties']['siteURL']
       
-      site_writer.writerow([site_name, wkt_coordinates,'nemsr', site_description])
+      site_writer.writerow([wkt_coordinates, "Australia", site_id, site_name,'NEMSR', site_description])
+  
+  total_count = total_count + counter
+
+print "Processed NEMSR records: " + str(total_count)
